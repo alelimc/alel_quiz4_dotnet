90790716d7797a30d79b03dd7e1963f62a23b66c
Aleli Macapagal <amacapag@my.centennialcollege.ca>
2025-04-07 12:28:31 -0400

    Initial commit

---
+++ alel_quiz4_webApp1/alel_quiz4_webApp1/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace alel_quiz4_webApp1
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            
+        }
+	}
+}
